230cfbe9800ca8f5675808d29d332788b76d7332
Fernando Balmaceda <phantom2024@gmail.com>
2019-11-14 16:24:02 -0300

    refactor de carpetas

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import { mutant } from "./controllers/mutants.controller";
 import { stats } from "./controllers/stats.controller";
-import { datastore } from "./controllers/datastore.controller";
+import { datastore } from "./triggers/pubsub";
 import { onSequenceSaved } from "./triggers/onSequenceSaved";
 
 export { mutant, stats, datastore, onSequenceSaved };
